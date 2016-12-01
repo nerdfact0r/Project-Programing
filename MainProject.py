fa762aa22581ab1498a258113b4ab43e61fe6d2d
nerdfact0r <easton.gallegos@gmail.com>
2016-12-01 10:03:02 -0700

    Update MainProject.py
    lets call scrooge wolf and here is my stuff

--- MainProject.py
+++ MainProject.py
@@ -1,4 +1,43 @@
-def MeetsUncleScrooge:
+def redintro():
+  print """
+  Red riding hood has always been happy. Her parents have loved
+  her and she's never gone to bed hungry.
+  """
+  raw_input()
+  print """
+  She's sitting with her parents at the table with a newspaper
+  spread out before them. It was showcasing the jobs that they
+  could take.
+  """
+  raw_input()
+  print"""
+  Her parents decide to take the far away job. They would be
+  gone for  two weeks. Red was happy though. Her parents loved
+  her and would be back soon.
+  """
+  raw_input()
+  print "two weeks later"
+  raw_input()
+  print """
+  Red was sitting next to the door waiting. Her parents had left
+  two weeks ago and were about to come home.
+  'You shouldn't be sitting next to the window miss' said the
+  maid, 'You'll catch a cold miss.' but Red didn't care. Her
+  parents were coming home.
+  """
+  raw_input()
+  print"""
+  There was a knock on the door. 'Mom! Dad!' shouted red as
+  she rushed to the door. But her parent's weren't there.
+  On the front step was a man holding a letter. As he handed
+  it to her he said 'my condolences miss'
+  """
+  raw_input()
+  print"""
+  Red wasn't happy anymore.
+  """
+
+def wolfIntro():
     print """
     You are laying in bed morning the loss of your parents and you can hear the
     slow drops of rain on your roof...
@@ -7,3 +46,8 @@ def MeetsUncleScrooge:
     print """
     Suddenly there is a knock on the door
     """
+    
+redintro()
+
+wolfIntro()
+
