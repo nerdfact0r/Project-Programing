687654a42bf46121021de5f6ac992fb7e6230fb4
Trevor <clydore@gmail.com>
2016-12-01 09:55:54 -0700

    uncle scrooge

---
+++ MainProject.py
@@ -0,0 +1,9 @@
+def MeetsUncleScrooge:
+    print """
+    You are laying in bed morning the loss of your parents and you can hear the
+    slow drops of rain on your roof...
+    """
+    raw_input()
+    print """
+    Suddenly there is a knock on the door
+    """
